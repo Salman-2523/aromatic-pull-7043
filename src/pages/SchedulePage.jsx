501e2205acb9d54a6c8c2e7ce50e07b410b6483f
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2022-08-27 00:31:05 +0530

    shedule page complete

--- src/pages/SchedulePage.jsx
+++ src/pages/SchedulePage.jsx
@@ -1,6 +1,7 @@
-import { Box, Container, Flex, Heading, Spacer, Text } from "@chakra-ui/react";
+import {  Box, Container, Heading } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import MatchField from "../components/shedulePage/MatchField";
 import SheduleTabs from "../components/shedulePage/SheduleTabs";
 import { getSheduleApi } from "../redux/sheduleReducer/actions";
 
@@ -19,50 +20,7 @@ const SchedulePage = () => {
       <SheduleTabs />
       <Box>
         {data.map((data, i) => {
-          return (
-            <Box key={i} margin="20px" >
-              <Flex gap="50px" boxShadow='base' padding='10px' textAlign='center'>
-                {" "}
-                <Box
-                  bg="red.700"
-                  fontSize="lg"
-                  fontWeight="bold"
-                  textAlign="center"
-                  color="white"
-                  padding='15px 10px 15px 10px' 
-                  borderRadius='5px'
-                 boxShadow='md'
-                  
-                >
-                  {data.matches[0].league == "Domestic" ? "DOM" : "INT"}
-                </Box>{" "}
-                <Heading size='md' >{data.seriesName}</Heading>{" "}
-                <Spacer/>
-                <Text fontSize='25px' color='gray' textAlign='center'>{'>'}</Text>
-              </Flex>
-              <Flex padding='10px' gap='10px'>
-                {
-                data.matches.map((data,i)=>{
-                    return(
-                        <Box key={i} boxShadow='md' minHeight='100px' >
-                            <Text>{data.matchType}</Text>
-                            <Flex>{data.venue}</Flex>
-                            <Flex>
-                                <Box>
-                                    {data.homeTeamName}
-                                </Box>
-                                <Box>
-                                    {data.awayTeamName}
-                                </Box>
-                            </Flex>
-
-                        </Box>
-                    )
-                })
-                }
-                </Flex>
-            </Box>
-          );
+          return <MatchField key={i} data={data} />;
         })}
       </Box>
     </Container>
