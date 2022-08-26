8f8cf6dfc3377b47add6abeaf2e6ed74cf71eea1
Muhammed Fahiz P A <32778039+fahisayub@users.noreply.github.com>
2022-08-26 10:33:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import "./App.css";
-<<<<<<< Updated upstream
+
 import { Footer } from "./components/Home/Footer";
 import { Navbar } from "./components/Home/Navbar";
 import { MainRoutes } from "./routes/MainRoutes";
-=======
 import News from "./components/News/News";
-//import { MainRoutes } from "./routes/MainRoutes";
->>>>>>> Stashed changes
 
 
 function App() {
@@ -16,16 +13,11 @@ function App() {
     <div className="App">
       <Navbar />
 
-<<<<<<< Updated upstream
       <MainRoutes />
       <Footer />
-=======
-      {/* <MainRoutes /> */}
 
-     <News />
 
->>>>>>> Stashed changes
-
+    
     </div>
   );
 }
