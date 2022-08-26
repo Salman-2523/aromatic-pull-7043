a7bf340a98b37057da19a45b53433faccc00837d
rajkumaryadav199 <kuraj10101999@gmail.com>
2022-08-26 11:22:52 +0530

    single page done
    single page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 
 import "./App.css";
-<<<<<<< Updated upstream
-import { Footer } from "./components/Home/Footer";
-import { Navbar } from "./components/Home/Navbar";
-import { MainRoutes } from "./routes/MainRoutes";
-=======
+// import { Footer } from "./components/Home/Footer";
+// import { Navbar } from "./components/Home/Navbar";
+// import { MainRoutes } from "./routes/MainRoutes";
 import News from "./components/News/News";
 //import { MainRoutes } from "./routes/MainRoutes";
->>>>>>> Stashed changes
-
-
 function App() {
 
   return (
     <div className="App">
-      <Navbar />
-
-<<<<<<< Updated upstream
-      <MainRoutes />
-      <Footer />
-=======
-      {/* <MainRoutes /> */}
-
-     <News />
-
->>>>>>> Stashed changes
-
-    </div>
+      {/* <Navbar />
+      <Footer /> */}
+{/* <MainRoutes /> */}
+<News />
+</div>
   );
 }
 
